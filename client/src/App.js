2083439a42701a604a1f7c695b0128c834de462e
Rediat <85253297+Dubnationrediat@users.noreply.github.com>
2023-06-05 02:34:18 +0530

    fist push

---
+++ client/src/App.js
@@ -0,0 +1,205 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer/Footer.js";
+import Header from "./components/Header/Header.js";
+import Login from "./Pages/LoginPage/Login.js";
+import SignUp from "./Pages/SignupPage/SignUp.js";
+import ForgotPassword from "./Pages/ForgotPasword/ForgotPassword.js";
+import PageNotFound from "./Pages/PageNotFound/PageNotFound.js";
+import ForNewPassword from "./Pages/NewPassword/NewPassword.js";
+import NewPassword from "./Pages/NewPassword/NewPassword.js";
+
+import ToUploadForPhd from "./Pages/PhdSectionFileUpload/PhdSectionFileUpload.js";
+import ToUploadForMtech from "./Pages/MtechSecitonFileUpload/MtechSecitonFileUpload.js";
+import ToUploadForBTech from "./Pages/BtechSectionFileUpload/BtechSectionFileUpload.js";
+
+import ToDisplayForBTech from "./Pages/BtechSectionFileDisplay/BtechSectionFileDisplay.js";
+import ToDisplayForPhd from "./Pages/PhdSecitonFileDisplay/PhdSecitonFileDisplay.js";
+import ToDisplayMtech from "./Pages/MtechSecitonFileDisplay/MtechSecitonFileDisplay.js";
+
+import Contribution from "./Pages/Contribute/Contribute";
+import HowItWorks from "./Pages/howItWorks/HowItWorks";
+
+import BuyOrSell from "./Pages/Products/BuyOrSell.js";
+import DashBord from "./Pages/dashBordComponents/DashBoard.js";
+import UpdateUserProfile from "./Pages/updateProfile/UpdateUserProfile.js";
+import DeleteUserPage from "./Pages/DeleteUserProfile/DeletePage.js";
+import UplaodNotifcations from "./Pages/UploadNotifications/UplaodNotifcations";
+import StudentInfo from "./Pages/StudentInfo/StudentInfo";
+import SellingGoods from "./Pages/uplaodSellGoods/UploadSellGoods.js";
+import YemishetuCollection from "./Pages/EkaZirzir/YemishetuCollection.jsx";
+import Otp from "./Pages/OTP/Otp";
+import PotectRoute from "./components/Protect/PotectRoute";
+import { axiosInstance } from "./Utility/axios";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "./components/Redux/Reducers/authSlice";
+function App() {
+  const dispatch = useDispatch();
+  const { isLoading } = useSelector((state) => state.auth);
+  useEffect(() => {
+    dispatch(getUser());
+  }, []);
+  if (isLoading) {
+    return <h1>loading</h1>;
+  }
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<SignUp />} />
+          <Route exact path="/home" element={<SignUp />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<SignUp />} />
+
+          <Route exact path="/contribution" element={<Contribution />} />
+          <Route exact path="/howitworks" element={<HowItWorks />} />
+          <Route exact path="/forgotPass" element={<ForgotPassword />} />
+          <Route exact path="/forgotPass/:email" element={<Otp />} />
+          <Route exact path="/ForNewPasswordOTP" element={<ForNewPassword />} />
+          <Route exact path="/setNewPassword" element={<NewPassword />} />
+          <Route
+            exact
+            path="/btechSectionUpload"
+            element={
+              <PotectRoute>
+                {" "}
+                <ToUploadForBTech />{" "}
+              </PotectRoute>
+            }
+          />
+          <Route
+            exact
+            path="/mtechSectioUpload"
+            element={
+              <PotectRoute>
+                <ToUploadForMtech />
+              </PotectRoute>
+            }
+          />
+          <Route
+            exact
+            path="/phdSectionUpload"
+            element={
+              <PotectRoute>
+                <ToUploadForPhd />
+              </PotectRoute>
+            }
+          />
+          <Route
+            exact
+            path="/btechSectionDisplay"
+            element={
+              <PotectRoute>
+                <ToDisplayForBTech />
+              </PotectRoute>
+            }
+          />
+          <Route
+            exact
+            path="/mtechSectioDisplay"
+            element={
+              <PotectRoute>
+                <ToDisplayMtech />
+              </PotectRoute>
+            }
+          />
+          <Route
+            exact
+            path="/phdSectionDisplay"
+            element={
+              <PotectRoute>
+                <ToDisplayForPhd />
+              </PotectRoute>
+            }
+          />
+          <Route
+            exact
+            path="/buyorsell"
+            element={
+              <PotectRoute>
+                <BuyOrSell />
+              </PotectRoute>
+            }
+          />
+          <Route
+            exact
+            path="/dashbord"
+            element={
+              <PotectRoute>
+                <DashBord />
+              </PotectRoute>
+            }
+          />
+          <Route
+            exact
+            path="/updateProfile"
+            element={
+              <PotectRoute>
+                <UpdateUserProfile />
+              </PotectRoute>
+            }
+          />
+          <Route
+            exact
+            path="/deleteProfile"
+            element={
+              <PotectRoute>
+                <DeleteUserPage />
+              </PotectRoute>
+            }
+          />
+          <Route
+            exact
+            path="/uploadNotification"
+            element={
+              <PotectRoute>
+                <UplaodNotifcations />
+              </PotectRoute>
+            }
+          />
+          <Route
+            exact
+            path="/studentInfo"
+            element={
+              <PotectRoute>
+                <StudentInfo />
+              </PotectRoute>
+            }
+          />
+          <Route
+            exact
+            path="/buyGoods"
+            element={
+              <PotectRoute>
+                <YemishetuCollection />
+              </PotectRoute>
+            }
+          />
+          <Route
+            exact
+            path="/sellGoods"
+            element={
+              <PotectRoute>
+                <SellingGoods />
+              </PotectRoute>
+            }
+          />
+          <Route
+            exact
+            path="*"
+            element={
+              <PotectRoute>
+                <PageNotFound />
+              </PotectRoute>
+            }
+          />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+import "./Header.css";
+import logo from "../../Images/logoIcon/IITR175.png";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { logout } from "../Redux/Reducers/authSlice";
+import {useNavigate } from "react-router-dom";
+import Cookies from "universal-cookie";
+
+function Header() {
+  const [display, setDisplay] = useState("Sign Up");
+  const [route, setRoute] = useState("signup");
+
+  const dispatch = useDispatch();
+  let { isAuth } = useSelector((state) => state.auth);
+  const cookie = new Cookies();
+  // *for navigation
+  // let navigate = useNavigate();
+
+  function clickHandler() {
+    if (display === "Sign Up") {
+      setRoute("login");
+      setDisplay("Log In");
+    } else {
+      setDisplay("Sign Up");
+      setRoute("signup");
+    }
+  }
+  useEffect(
+    (e) => {
+      if (e && e.preventDefault()) {
+        clickHandler(e);
+        console.log("its running again");
+      }
+    },
+    [route, display]
+  );
+  const logoutHandler = (e) => {
+    dispatch(logout());
+    cookie.remove("token");
+    // navigate("/login");
+    // window.location.href='/login'
+  };
+
+  return (
+    <div className="header container-fluid">
+      <div className="innerContainer container d-flex justify-content-around ">
+        <div className="header__image">
+          <img src={logo} alt="IIT ROORKEE logo" />
+        </div>
+        <button className="ic d-sm-block d-md-none">☰</button>
+        <div className="d-flex   innerContainer2 justify-content-between d-none d-md-block">
+          {/* <Link to="/">Home</Link> */}
+          <Link to="https://acad.iitr.ac.in/Default.aspx?ReturnUrl=%2fStudent%2fDefault.aspx&AspxAutoDetectCookieSupport=1">
+            IITR Portal
+          </Link>
+          <Link to="https://ir.iitr.ac.in/IR_Panel/">IR Portal</Link>
+          <Link to="/howitworks">How It Works</Link>
+          <Link to="/contribution">Contribute</Link>
+          <Link to="/newInformation">Information</Link>
+          <Link to={isAuth ? "/dashbord" : "/login"} className="loginHeader">
+            {isAuth ? "Dashboard" : "Login"}
+          </Link>
+          {!isAuth && (
+            <Link  to={"/signup"}>
+              <Button className="loginHeader forText">SignUp</Button>
+            </Link>
+          )}
+          {isAuth && (
+            <Link>
+              <Button className=" loginHeader forText" onClick={logoutHandler}>Logout</Button>
+            </Link>
+          )}
+       
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
