10ae08edcccc823686a6003c9abb29262f6156a6
mesayaredo <mesayaredo@gmail.com>
2023-07-04 00:25:48 -0600

    update

---
+++ client/src/Pages/Contribute/Contribute.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Contribute.css'
+
+function Contribute() {
+  return (
+    <div>Contribute</div>
+  )
+}
+
+export default Contribute;
---
+++ client/src/Pages/PageNotFound/PageNotFound.js
@@ -0,0 +1,17 @@
+
+
+
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './PageNotFound.css'
+function PageNotFound() {
+  return (
+    <div className='pageNotFound my-4'>
+          <h1 className=''>Ooops..... Page Not Found !!</h1>
+          <Link className='thankYouAnch' href='/signup'>Click To SignUp</Link>
+    </div>
+  )
+}
+
+export default PageNotFound
